e866a0b3edbf0d2112a4768d705bdf378b862906
kawaiizenbo <48113593+kawaiizenbo@users.noreply.github.com>
2022-04-18 11:37:44 -0700

    Add project files.

---
+++ netrautafarmi/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace netrautafarmi
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.postButton = new System.Windows.Forms.Button();
+            this.nicknameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.messagesView = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(76, 300);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(496, 20);
+            this.textBox.TabIndex = 1;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(472, 326);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(100, 23);
+            this.refreshButton.TabIndex = 2;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // postButton
+            // 
+            this.postButton.Location = new System.Drawing.Point(366, 326);
+            this.postButton.Name = "postButton";
+            this.postButton.Size = new System.Drawing.Size(100, 23);
+            this.postButton.TabIndex = 3;
+            this.postButton.Text = "Post";
+            this.postButton.UseVisualStyleBackColor = true;
+            this.postButton.Click += new System.EventHandler(this.postButton_Click);
+            // 
+            // nicknameBox
+            // 
+            this.nicknameBox.Location = new System.Drawing.Point(76, 328);
+            this.nicknameBox.Name = "nicknameBox";
+            this.nicknameBox.Size = new System.Drawing.Size(284, 20);
+            this.nicknameBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nickname:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Message:";
+            // 
+            // messagesView
+            // 
+            this.messagesView.Location = new System.Drawing.Point(12, 12);
+            this.messagesView.Name = "messagesView";
+            this.messagesView.Size = new System.Drawing.Size(560, 282);
+            this.messagesView.TabIndex = 8;
+            this.messagesView.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.messagesView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nicknameBox);
+            this.Controls.Add(this.postButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.textBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "netrautafarmi";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button postButton;
+        private System.Windows.Forms.TextBox nicknameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox messagesView;
+    }
+}
+
---
+++ netrautafarmi/MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace netrautafarmi
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            using(WebClient wc = new WebClient())
+            {
+                string html = wc.DownloadString("http://donut.gq/rautafarmi/messages.txt");
+                messagesView.Text = Regex.Replace(html, "<.*?>", "");
+            }
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                string html = wc.DownloadString("http://donut.gq/rautafarmi/messages.txt");
+                messagesView.Text = Regex.Replace(html, "<.*?>", "");
+            }
+        }
+
+        private void postButton_Click(object sender, EventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var data = new NameValueCollection();
+                data["name"] = nicknameBox.Text;
+                data["msg"] = textBox.Text;
+                wc.UploadValues("https://donut.gq/rautafarmi/post.php", data);
+                string html = wc.DownloadString("http://donut.gq/rautafarmi/messages.txt");
+                messagesView.Text = Regex.Replace(html, "<.*?>", "");
+            }
+        }
+    }
+}
